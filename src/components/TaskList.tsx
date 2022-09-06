119a07c026a115d082e36a01d2e0e80a157372d3
Lucas Miranda <lucas.miranda@liferay.com>
2022-09-06 18:40:50 -0300

    function to add a new task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,23 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+  function handleCreateNewTask() {    
+    const randomId = Number(Math.floor(Math.random() * 1000000));
+    const newCreatedTask: Task = {
+      id: randomId,
+      title: newTaskTitle,
+      isComplete: false,
+    };
+
+    setTasks([...tasks, newCreatedTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    
   }
 
   return (
